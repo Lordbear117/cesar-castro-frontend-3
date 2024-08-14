7cc6d7ea1b3e49ade00a00c871107385ea987e0a
Lordbear117 <blackyi_14@hotmail.com>
2024-08-14 00:46:49 +0000

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Card from './Components/Card'
+import './App.css'
+
+function App() {
+  return (
+    <div className='App'>
+      <h1>Carga de estudiantes</h1>
+      <form></form>
+      <Card />
+    </div>
+  )
+}
+
+export default App
